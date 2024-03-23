c8f8a15a61b3bb8de36523c6acd88d188c1fe02b
lilansheng <945004642@qq.com>
2024-03-23 10:22:17 +0800

    配置别名路径@

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
+import sum from '@/sum'
+const sum1 = sum(1, 2)
+console.log('sum: ', sum1)
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(<App />)
