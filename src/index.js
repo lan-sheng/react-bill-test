d8e56d06604775258a92236b8db04ffd27d71faf
lilansheng <945004642@qq.com>
2024-03-23 16:15:22 +0800

    主题定制化

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import sum from '@/sum'
 import { RouterProvider } from 'react-router-dom'
 import router from './router'
+import './theme.css'
+
+import sum from '@/sum'
 const sum1 = sum(1, 2)
 console.log('sum: ', sum1)
 
--- src/pages/Layout/index.js
+++ src/pages/Layout/index.js
@@ -1,9 +1,15 @@
+import { Button } from 'antd-mobile'
 import { Outlet } from 'react-router-dom'
 
 const Layout = () => {
   return (
     <div>
-      Layout <Outlet/>
+      Layout
+      <Button color="primary">全局样式</Button>
+      <div className="puple">
+        <Button color="primary">局部样式</Button>
+      </div>
+      <Outlet />
     </div>
   )
 }
