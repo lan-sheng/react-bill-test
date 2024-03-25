446d0b8879fc80422af82fb0d59ca4a319ec11c2
lilansheng <945004642@qq.com>
2024-03-25 15:58:36 +0800

    月度账单-统计区域静态结构搭建

--- src/pages/Month/index.js
+++ src/pages/Month/index.js
@@ -1,4 +1,55 @@
+import { NavBar, DatePicker } from 'antd-mobile'
+import './index.scss'
+import classNames from 'classnames'
 const Month = () => {
-  return <div>month</div>
+  return (
+    <div className="monthlyBill">
+      <NavBar className="nav" backArrow={false}>
+        月度收支
+      </NavBar>
+      <div className="content">
+        <div className="header">
+          {/* 时间切换区域 */}
+          <div className="date" onClick={() => setDateVisible(true)}>
+            {/* <span className="text">{currentDate + ''}月账单</span> */}
+            <span className="text">{ + ''}月账单</span>
+            {/* 思路：根据当前弹框打开的状态控制expand类名是否存在 */}
+            {/* <span className={classNames('arrow', dateVisible && 'expand')}></span> */}
+            <span className={classNames('arrow', 'expand')}></span>
+          </div>
+          {/* 统计区域 */}
+          <div className="twoLineOverview">
+            <div className="item">
+              {/* <span className="money">{monthResult.pay.toFixed(2)}</span> */}
+              <span className="type">支出</span>
+            </div>
+            <div className="item">
+              {/* <span className="money">{monthResult.income.toFixed(2)}</span> */}
+              <span className="type">收入</span>
+            </div>
+            <div className="item">
+              {/* <span className="money">{monthResult.total.toFixed(2)}</span> */}
+              <span className="type">结余</span>
+            </div>
+          </div>
+          {/* 时间选择器 */}
+          {/* <DatePicker
+            className="kaDate"
+            title="记账日期"
+            precision="month"
+            visible={dateVisible}
+            onCancel={() => setDateVisible(false)}
+            onConfirm={onConfirm}
+            onClose={() => setDateVisible(false)}
+            max={new Date()}
+          /> */}
+        </div>
+        {/* 单日列表统计 */}
+        {/* {dayGroup.keys.map(key => {
+          return <DailyBill key={key} date={key} billList={dayGroup.groupData[key]} />
+        })} */}
+      </div>
+    </div>
+  )
 }
 export default Month
